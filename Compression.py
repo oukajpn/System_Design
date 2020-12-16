d023240a81d6477a0625ba4c09637e0bec51d559
oukajpn <ouka.jpn.local@gmail.com>
2020-12-17 07:53:49 +0900

    fixes

--- Compression.py
+++ Compression.py
@@ -2,16 +2,9 @@
 from time import sleep
 import Detection
 import Push_Pin
-#import time
 import RPi.GPIO as GPIO
 
 health = 0
-#danger = Detection.safe
-#max_elevat = 0
-#now_elevat = 0
-#add_elevat = 0
-#pb_state = Push_Pin.state
-#pb_cnt = 0
 
 #stepper variables
 DIR = 20
@@ -25,68 +18,45 @@ GPIO.output(DIR, 1)
 step_count = 200
 delay = .001
 
-'''
-if(pb_state==1):
-    if(pb_cnt == 0):
-        pb_cnt = 1
-        while(max_elevat > now_elevat):
-            if(danger == 0):
-                for x in range(step_count):
-                    GPIO.output(STEP, GPIO.HIGH)
-                    sleep(delay)
-                    GPIO.output(STEP, GPIO.LOW)
-                    sleep(delay)
-                now_elevat -= add_elevat
-                GPIO.cleanup()
-        now_elevat = 0
-        health = 1
-    else:
-        while(max_elevat > now_elevat):
-            if(danger == 0):
-                for x in range(step_count):
-                    GPIO.output(STEP, GPIO.HIGH)
-                    sleep(delay)
-                    GPIO.output(STEP, GPIO.LOW)
-                    sleep(delay)
-                now_elevat -= add_elevat
-                GPIO.cleanup()
-        now_elevat = 0
-        pb_cnt = 0
-'''
+def push(max_elevat,now_elevat,add_elevat):
+    while(max_elevat > now_elevat):
+        danger = Detection.detection()
+        if(danger == 0):
+            for x in range(step_count):
+                GPIO.output(STEP, GPIO.LOW)
+                sleep(delay)
+                GPIO.output(STEP, GPIO.HIGH)
+                sleep(delay)
+            now_elevat -= add_elevat
+            GPIO.cleanup()
+
+def down(max_elevat,now_elevat,add_elevat):
+    while(max_elevat > now_elevat):
+        danger = Detection.detection()
+        if(danger == 0):
+            for x in range(step_count):
+                GPIO.output(STEP, GPIO.HIGH)
+                sleep(delay)
+                GPIO.output(STEP, GPIO.LOW)
+                sleep(delay)
+            now_elevat -= add_elevat
+            GPIO.cleanup()
 
 def compression():
     global health
     max_elevat = 0
     now_elevat = 0
     add_elevat = 0
-    danger = Detection.detection()
     pb_state = Push_Pin.detec_btn()
     pb_cnt = 0
     
     if(pb_state==1):
         if(pb_cnt == 0):
             pb_cnt = 1
-            while(max_elevat > now_elevat):
-                if(danger == 0):
-                    for x in range(step_count):
-                        GPIO.output(STEP, GPIO.LOW)
-                        sleep(delay)
-                        GPIO.output(STEP, GPIO.HIGH)
-                        sleep(delay)
-                    now_elevat -= add_elevat
-                    GPIO.cleanup()
+            push(max_elevat,now_elevat,add_elevat)
             now_elevat = 0
             health = 1
-
         else:
-            while(max_elevat > now_elevat):
-                if(danger == 0):
-                    for x in range(step_count):
-                        GPIO.output(STEP, GPIO.HIGH)
-                        sleep(delay)
-                        GPIO.output(STEP, GPIO.LOW)
-                        sleep(delay)
-                    now_elevat -= add_elevat
-                    GPIO.cleanup()
+            down(max_elevat,now_elevat,add_elevat)
             now_elevat = 0
             pb_cnt = 0
